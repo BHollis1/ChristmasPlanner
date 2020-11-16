3395ed7e1daf3729366e3debf21e9477d5ee781b
BHollis1 <betsy.hollis1@gmail.com>
2020-11-16 10:12:16 -0500

    Add project files.

---
+++ ChristmasPlanner.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChristmasPlanner.WebMVC.MvcApplication" Language="C#" %>
---
+++ ChristmasPlanner.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChristmasPlanner.WebMVC.Startup))]
+namespace ChristmasPlanner.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
