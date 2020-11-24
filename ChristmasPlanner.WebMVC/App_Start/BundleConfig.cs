687b326d6612c018f6596b972a434bef11d597ff
BHollis1 <betsy.hollis1@gmail.com>
2020-11-24 16:21:12 -0500

    More CSS changes

--- ChristmasPlanner.WebMVC/App_Start/BundleConfig.cs
+++ ChristmasPlanner.WebMVC/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace ChristmasPlanner.WebMVC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            
+
         }
     }
 }
